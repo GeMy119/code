a7a3fd24968e57aca6da85efdcbd3df403c7a2c3
GeMy119 <mahmoud.gamal1191998@gmail.com>
2024-09-27 16:06:45 +0300

    add header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'منصة وسيط لخدمات التعميد';
+export class AppComponent implements OnInit {
+  title = 'منصة العمل الحر | الرئيسية';
+  isLoading: boolean = true;
+
+  ngOnInit() {
+    // هنا يمكنك محاكاة تحميل البيانات باستخدام setTimeout
+    setTimeout(() => {
+      this.isLoading = false;  // تغيير حالة التحميل بعد 3 ثواني كمثال
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WasetQrcodeComponent } from './waset-qrcode/waset-qrcode.component';
+import { LoadingComponent } from './loading/loading.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WasetQrcodeComponent
+    WasetQrcodeComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
